c52396f002117f2d6bafc770f865e5ddee5b8cdd
Godfrey18 <godfreymenezes07@gmail.com>
2020-03-06 18:31:44 +0530

    cleanupp Apllication

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+	has_many :user_stocks
+	has_many :users, through: :user_stocks
+	validates :name,:ticker,presence:true
+	
 	def self.new_lookup(ticker_symbol)
 		client = IEX::Api::Client.new(publishable_token: 'Tpk_0afc48f949b74aabba3a146c1d70694e',
 			secret_token: 'Tsk_9ee7470d068645d389d1ab8ffd75a723',
